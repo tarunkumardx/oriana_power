967ab9f267e4f6576a85cb44b18b96053e2155cc
Tarun Kumar <tarunkumar@Taruns-MacBook-Air.local>
2024-10-16 15:24:24 +0530

    oriana mockup

---
+++ src/components/landingPageVideo.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+const LandingPageVideo = () => {
+  return (
+    // <div style={{ marginTop: "-2rem" }}>
+    //   <video width="100%" height="100%" autoPlay muted loop>
+    //     <source
+    //       src="https://www.shutterstock.com/shutterstock/videos/1090773539/preview/stock-footage-ecology-solar-power-station-panels-in-the-fields-green-energy-at-sunset-landscape-electrical.webm"
+    //       type="video/webm"
+    //     />
+    //     Your browser does not support the video tag.
+    //   </video>
+    //   <div className="mainBannerText">
+    //     Goal - <span> Expand renewable</span>
+    //     <br />
+    //     <span>energy </span>capacity to <strong>10 GW by 2030.</strong>
+    //   </div>
+    // </div>
+
+    <div style={{ position: "relative", overflow: "hidden" }}>
+      <video
+        style={{
+          width: "100%",
+          height: "90vh", // or "100vh" for full height
+          objectFit: "cover",
+        }}
+        autoPlay
+        muted
+        loop
+      >
+        <source
+          src="https://www.shutterstock.com/shutterstock/videos/1090773539/preview/stock-footage-ecology-solar-power-station-panels-in-the-fields-green-energy-at-sunset-landscape-electrical.webm"
+          type="video/webm"
+        />
+        Your browser does not support the video tag.
+      </video>
+      <div className="mainBannerText">
+        Goal - <span> Expand renewable</span>
+        <br />
+        <span>energy </span>capacity to <strong>10 GW by 2030.</strong>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPageVideo;
